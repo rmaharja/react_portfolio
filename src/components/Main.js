4adfc673c00677fef941fc76471a400677ea64f2
rmaharja@gmail.com <rmaharja@gmail.com>
2018-11-11 10:45:04 -0500

    navbar added

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Landing from "Landing";
+
+
+const Main = () => {
+  <Switch>
+    <Route exact path="/" component={Landing}></Route>
+  </Switch>
+}
+
+export default Main;
---
+++ src/components/Summary.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Summary extends Component {
+  render(){
+    return(
+      <div>
+        <h1>
+          Resume/Summary
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Summary;
