497bdd6b4c8a2a1d0016a3b5c6f621ded1d1f918
rmaharja@gmail.com <rmaharja@gmail.com>
2018-11-11 17:35:16 -0500

    working on landing header

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {Layout, Header, Navigation, Drawer, Content} from "react-mdl";
 import {Link} from "react-router-dom";
 import Main from "./components/Main";
 
 class App extends Component {
-  render() {
-    return (
-<div className="demo-big-content">
-    <Layout>
-        <Header title="Rohit" scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Rohit">
-            <Navigation>
-            <Link to="/resume">Resume</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-</div>
-    );
-  }
+    render() {
+        return (
+            <div className="demo-big-content">
+                <Layout>
+                    <Header title="Rohit's Portfolio" className="header"scroll>
+                        <Navigation>
+                            <Link to="/">Home</Link>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title="Rohit's Portfolio">
+                        <Navigation>
+                            <Link to="/">Home</Link>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content"/>
+                        <Main/>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,21 @@
 import React, {Component} from "react";
+import {Grid, Cell} from "react-mdl";
+import  Rohit from "../images/Rohit.jpg";
 
 class Landing extends Component {
   render(){
     return(
-      <div>
-        <h1>
-          Landing
-        </h1>
+      <div style={{width: "100%", margin: "auto"}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img src="http://res.cloudinary.com/cloudrohit/image/upload/r_30/a_270/v1541513598/plrht0u1u7l2eejmvdxf.png" 
+            alt="profile-pic" id="profile-pic"/>
+
+            <label for="profile-pic" id="banner-header">
+            <h1>Full Stack Web Developer</h1>
+            </label>
+          </Cell>
+        </Grid>
       </div>
     )
   }
